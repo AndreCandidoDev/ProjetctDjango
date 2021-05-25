4d5338130c536f15b8b63e4a0cd28c26d48cc065
andre <andd68@hotmail.com>
2021-05-25 14:32:39 -0300

    add google and twitter providers

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.twitter',
 
     'bootstrapform',
     'clientes',
